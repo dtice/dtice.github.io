64ca43d3f2eb24f9f16d27406fbb132d368a9b54
Dillon Tice <Dillon Tice>
2016-11-18 08:16:33 -0700

    Fancy animations

--- js/Face.js
+++ js/Face.js
@@ -4,28 +4,37 @@ $(document).ready(function(){
 	var rightOpen = "static/img/rightfaceopen.gif";
 	var rightClosed = "static/img/rightfaceclosed.gif";
 	var nIntervId;
+	var faceNum = 5;
 	var t = 0;
 	var flip = false;
 	$("#stop").click(function(){
 		$("audio").stop();
 	});
+	for(var i = 1; i <= faceNum; i++)
+	{
+		$("#faceColumn").prepend('<img id="face' + i + '" src="static/img/rightfaceclosed.gif" />');
+	}
 	eat();
-	moveit();
+	moveit(faceNum);
 	
-	function moveit() {
+	function moveit(num) {
 		t += 0.05;
 
 		var r = .5 * $("body").height();         // radius
 		var xcenter = 0;   // center X position
 		var ycenter = (.5 * $("body").height());   // center Y position
 
-		var newTop = Math.floor(ycenter + (r * Math.sin(t)));
-
-		$('#face').animate({
+		
+		for(var i = 1; i <= num; i++)
+		{
+			var newTop = Math.floor(ycenter + (r * Math.sin((t * i)/2)));
+			console.log("#face"+i);
+		$("#face" + i).animate({
 			top: newTop,
 		}, 1, function() {
-			moveit();
+			moveit(faceNum);
 		});
+		}
 	}
 	function eat(){
 		nIntervId = setInterval(eating, 200);
@@ -33,13 +42,17 @@ $(document).ready(function(){
 	function eating(){
 		if(!flip)
 		{
-			$("#face").attr("src", rightOpen);
-			
+			for(var i = 1; i <= faceNum; i++)
+			{
+				$("#face" + i).attr("src", rightOpen);
+			}
 			flip = true;
 		}
 		else{
-			$("#face").attr("src", rightClosed);
-			
+			for(var i = 1; i <= faceNum; i++)
+			{
+				$("#face" + i).attr("src", rightClosed);
+			}
 			flip = false;
 		}
 	}
