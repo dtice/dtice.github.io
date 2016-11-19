15bd3d8f2d5994c7ca99c68e86d2055237d55225
Dillon Tice <Dillon Tice>
2016-11-19 04:03:50 -0700

    Webkins 1.1

--- js/Face.js
+++ js/Face.js
@@ -4,13 +4,44 @@ $(document).ready(function(){
 	var rightOpen = "static/img/rightfaceopen.gif";
 	var rightClosed = "static/img/rightfaceclosed.gif";
 	var nIntervId;
+	var flipInterv;
+	var width = $("document").width();
 	var faceNum = 5;
 	var t = 0;
+	var flippy = false;
+	var floppy = false;
 	var flip = false;
-	$("#stop").click(function(){
-		$.each($('audio'), function () {
-    this.pause();
-});
+	var flop = false;
+	$("#illuminati").css("left:"+width);
+	$("#illuminati").click(function(){
+		if(!floppy)
+		{
+			$.each($('audio'), function () {
+				this.pause();
+			});
+			floppy = true;
+		}
+		else
+		{
+			$.each($('audio'), function () {
+				this.play();
+			});
+			floppy = false;
+		}
+	});
+	$("#contactButton").click(function(){
+		if(!flippy)
+		{
+			$("#contactBar").slideDown( "slow", function() {
+			});
+			flippy = true;
+		}
+		else
+		{
+			$("#contactBar").slideUp("slow", function() {
+			});
+			flippy = false;
+		}
 	});
 	for(var i = 1; i <= faceNum; i++)
 	{
@@ -18,6 +49,34 @@ $(document).ready(function(){
 	}
 	eat();
 	moveit(faceNum);
+	flippem();
+	
+	function flippem(){
+		flipInterv = setInterval(flipping, 400);
+	}
+	
+	function randBoolean(){
+		var rando = false;
+		if(Math.random() >= 0.5)
+		{
+			rando = true;
+		}
+		return rando;
+	}
+	
+	function flipping(){
+		for(var i = 1; i <= faceNum; i++)
+		{
+			if($("#face"+i).hasClass("flip") && randBoolean())
+			{
+				$("#face"+i).removeClass("flip");
+			}
+			else
+			{
+				$("#face"+i).addClass("flip");
+			}
+		}
+	}
 	
 	function moveit(num) {
 		t += 0.05;
@@ -30,7 +89,6 @@ $(document).ready(function(){
 		for(var i = 1; i <= num; i++)
 		{
 			var newTop = Math.floor(ycenter + (r * Math.sin((t * i)/2)));
-			console.log("#face"+i);
 		$("#face" + i).animate({
 			top: newTop,
 		}, 1, function() {
