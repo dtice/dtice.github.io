61678e15a257b0fa77818809d358e25fc159d00e
Dill <dillon.tice@gmail.com>
2018-02-01 17:58:22 -0700

    Writ 221 Syllabus Website Initial Commit

--- js/gitStub.js
+++ js/gitStub.js
@@ -1,20 +1,34 @@
 /* Retrieves 5 most recent contributions from set GitHub user */
 $(document).ready(function() {
-    something("gitStub0");
-    something("gitStub1");
-    something("gitStub2");
-    something("gitStub3");
-    something("gitStub4");
-    something("gitStub5");
+    another();
 });
 
-function something(item){
+function mdToHTML(item, index){
+    function poster(){
+    var converter = new showdown.Converter();
+    $('#gitStub' + index).append('<div class="readme">' + converter.makeHtml(this.responseText) + '</>');
+    }
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            document.getElementById(item).innerHTML = "<h3>Title goes here</h3>" + "<br>" + this.responseText;
-        }
-    };
-    xhttp.open("GET", "https://raw.githubusercontent.com/bagder/libbrotli/master/README.md", true);
+    xhttp.addEventListener("load", poster);
+    xhttp.open("GET", item, true);
     xhttp.send();
 }
+
+function another(){
+    $.ajax({
+        
+    url: "https://api.github.com/users/dtice/repos",
+    dataType: "text",
+    
+    success: function(data) {
+        
+        var json = $.parseJSON(data);
+        
+        for (var i=0;i<json.length;++i)
+        {
+            mdToHTML('https://raw.githubusercontent.com/dtice/'+json[i].name+'/master/README.md', i);
+        }
+    }
+});
+}
+
